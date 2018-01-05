e9ba12c4d48c9cc2cd9634e83dc60527defbf2df
Losmli010 <33534770+Losmli010@users.noreply.github.com>
2018-01-05 14:04:29 +0800

    Update and rename text_gbk2utf8.py to text_gb2utf8.py

--- tool/text_gb2utf8.py
+++ tool/text_gb2utf8.py
@@ -2,24 +2,19 @@
 
 import codecs
 
-def read_file(path,encoding="gbk"):
+def read_file(path,encoding="gb18030"):
     file=codecs.open(path,"r",encoding)
-    text=[]
-    for line in file:
-        line=line.strip()
-        if line:
-            text.append(line)
+    text=[line.strip() for line in file if line]
     file.close()
     return text
 
 def write_file(path,text,encoding="utf-8"):
     file=codecs.open(path,"a",encoding)
-    content=[line+"\n" for line in text]
-    for line in content:
+    for line in text:
+        line=line+"\n"
         file.write(line)
     file.close()
 
 def gbk2utf8(src,dst):
-    text = read_file(src,encoding="gbk")
+    text = read_file(src,encoding="gb18030")
     write_file(dst,text,encoding="utf-8")
-
