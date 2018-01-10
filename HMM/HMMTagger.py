b112872fffcdea827a85a4c9b107fddae8c0c2bb
Losmli010 <33534770+Losmli010@users.noreply.github.com>
2018-01-10 14:02:51 +0800

    Update HMMTagger.py

--- HMM/HMMTagger.py
+++ HMM/HMMTagger.py
@@ -5,15 +5,15 @@ import pickle
 from viterbi import viterbi
 
 def tagger(sentence):
-    Pi,A,B=load_parameters("data/HMMTagger.parameters.npz")
+    Pi,A,B = load_parameters("data/HMMTagger.parameters.npz")
     with open("data/Taggerindex.pkl","rb") as f:
-        word_index=pickle.load(f)
-        label_index=pickle.load(f)
+        word_index = pickle.load(f)
+        label_index = pickle.load(f)
     f.close()
-    obs=map_obs(word_index,sentence)
-    prob,route=viterbi(obs,Pi,A,B)
-    sequence=[label_index[i] for i in route]
-    result=''
+    obs = map_obs(word_index,sentence)
+    prob,route = viterbi(obs,Pi,A,B)
+    sequence = [label_index[i] for i in route]
+    result = ''
     for word,tag in zip(sentence,sequence):
-        result+=''.join(' '+word+'/'+tag+' ')
+        result += ''.join(' '+word+'/'+tag+' ')
     return result.strip()
