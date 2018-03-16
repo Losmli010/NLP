80e284391356917d59a98c813443382c22ba74c8
Losmli010 <33534770+Losmli010@users.noreply.github.com>
2018-03-16 15:08:57 +0800

    Update utils.py

--- classification/cnn/utils.py
+++ classification/cnn/utils.py
@@ -4,26 +4,13 @@ import numpy as np
 import re
 
 def clean_str(string):
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r",", " , ", string)
-    string = re.sub(r"!", " ! ", string)
-    string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
-    string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
-    return string.strip().lower()
+    string = re.sub(r"[^\u4E00-\u9FA5，！？。\“\”《》A-Za-z0-9(),!?\'\`]", " ", string)
+    return string.strip()
 
 def load_data(positive_sentiment_file, negative_sentiment_file):
     positive_sentiments = [line.strip() for line in open(positive_sentiment_file, "r", encoding="utf-8")]
     negative_sentiments = [line.strip() for line in open(negative_sentiment_file, "r", encoding="utf-8")]
     x_text = positive_sentiments + negative_sentiments
-    x_text = [clean_str(sent) for sent in x_text]
     positive_labels = [[0, 1] for _ in positive_sentiments]
     negative_labels = [[1, 0] for _ in negative_sentiments]
     y = np.concatenate([positive_labels, negative_labels], 0)
@@ -43,3 +30,47 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
             yield shuffled_data[start_index:end_index]
+
+def get_vocab(filename, text):
+    words = [word for sent in text for word in sent.split()]
+    vocab = set(words)
+    with open(filename, "w", encoding="utf-8") as f:
+        for w in vocab:
+            f.write(w + "\n")
+    print("Save vocab to %s" %filename)
+    
+def load_vocab(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        vocab = [line.strip() for line in f]
+        vocab_dict = dict(zip(vocab, range(len(vocab))))
+    return vocab, vocab_dict
+
+def fit_transform(text, vocab_dict):
+    max_sentence_length = max([len(x.split()) for x in text])
+    text2num = []
+    for sent in text:
+        sent2num = [vocab_dict[word] for word in sent.split()]
+        pooled_length = max_sentence_length - len(sent2num)
+        sent2num += list(np.zeros((pooled_length)))
+        text2num.append(sent2num)
+    return text2num
+                         
+def load_words_vector(filename, vocab):
+    word2vec_dict = {}
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            tokens = line.strip().split()
+            word = tokens[0]
+            vector = tokens[1:]
+            if word in vocab:
+                word2vec_dict[word] = [float(x) for x in vector]
+    embedding_dim = len(vector)
+    print("Found {} out of {} in word2vec." .format(len(word2vec_dict), len(vocab)))
+    return word2vec_dict,embedding_dim
+
+def build_initial_embedding_matrix(vocab_dict, word2vec_dict, embedding_dim):
+    initial_embeddings = np.random.uniform(-1.0, 1.0, (len(vocab_dict), embedding_dim))
+    for word, index in vocab_dict.items():
+        if word in word2vec_dict:
+            initial_embeddings[index, :] = word2vec_dict[word]
+    return initial_embeddings
